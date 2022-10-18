8bf453edd2e801c2d5c61dae86f135d58ffd02d6
AkutoBlade <rared.isaacs@gmail.com>
2022-10-18 16:06:42 +0200

    Work

--- index.js
+++ index.js
@@ -8,6 +8,29 @@ const usersRoute = require("./routes/usersRoute");
 app.set("port", process.env.PORT || 1517);
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin","*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-*", "*");
+  next();
+});
+
+app.use(
+  cors({
+    mode: 'no-cors',
+    origin: ["http://localhost:8080/", "http://127.0.0.1:8080"],
+    credentials: true,
+  })
+);
+
+app.use(
+  router,
+  express.json(),
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.use(express.static("public"));
 
